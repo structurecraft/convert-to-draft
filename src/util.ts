92e7d1adbcb28c94abcbba264942efd019a886fe
Callum Sykes <28632338+clicketyclackety@users.noreply.github.com>
2022-02-24 08:06:46 -0800

    Delete util.ts

--- src/util.ts
+++
@@ -1,9 +0,0 @@
-export function shouldConvertToDraft(
-  timestamp: string,
-  num_days: number
-): boolean {
-  const daysInMillis = 1000 * 60 * 60 * 24 * num_days
-  const millisSinceLastUpdated =
-    new Date().getTime() - new Date(timestamp).getTime()
-  return millisSinceLastUpdated > daysInMillis
-}
